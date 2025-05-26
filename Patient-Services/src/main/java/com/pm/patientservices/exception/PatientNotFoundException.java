a00389852d5ab692514ed216b22111b4e1dba1f6
Aditya Gupta <aditya.gupta93117@gmail.com>
2025-05-26 21:32:05 +0530

    added updated patient method

---
+++ Patient-Services/src/main/java/com/pm/patientservices/exception/PatientNotFoundException.java
@@ -0,0 +1,8 @@
+package com.pm.patientservices.exception;
+
+public class PatientNotFoundException extends RuntimeException {
+
+	public PatientNotFoundException(String message) {
+		super(message);
+	}
+}
